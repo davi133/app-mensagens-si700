baad65512425d6a8f1ffca5ac9d2d0bdeff7b38f
davi133 <dqavi13@gmail.com>
2022-10-11 00:28:18 -0300

    tela de contato agora tem form de novo contato

--- lib/View/contato_tela.dart
+++ lib/View/contato_tela.dart
@@ -28,7 +28,13 @@ class TelaContatos extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 FloatingActionButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (ctx){
+                        return NewContactScreen();
+                      })
+                    );
+                  },
                   tooltip: 'Increment',
                   child: const Icon(Icons.add),
                 ),
@@ -36,6 +42,88 @@ class TelaContatos extends StatelessWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+
+class NewContactScreen extends StatelessWidget {
+  NewContactScreen({Key? key}) : super(key: key);
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  Contato aux = Contato("", 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("novo contato")),
+      body: Container(
+        margin: const EdgeInsets.all(15.0),
+        padding: const EdgeInsets.all(3.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              ConstrainedBox(
+                constraints: const BoxConstraints(minHeight: 80,minWidth: 300,maxWidth: 500),
+                child:
+                  SizedBox(
+                    child: TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: 'Apelido',
+                      ),
+                      validator: (String? inValue) {
+                        if (inValue != null) {
+                          if (inValue.isEmpty) {
+                            return "Insira um apelido";
+                          }
+                        }
+                        return null;
+                      },
+                      onSaved: (String? inValue) {
+                        aux.apelido = inValue ?? "";
+                      },
+                    ),
+                  ),
+                
+              ),
+               ConstrainedBox(
+                constraints: const BoxConstraints(minHeight: 85,minWidth: 300,maxWidth: 500),
+                child:
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: 'Número',
+                    ),
+                    validator: (String? inValue) {
+                      if (inValue != null) {
+                        if (inValue.isEmpty) {
+                          return "Insira um número de contato";
+                        }
+                        if (!RegExp(r'^[0-9]+$').hasMatch(inValue)) {
+                          return "Insira Apenas números";
+                        }
+                      }
+                      return null;
+                    },
+                    onSaved: (String? inValue) {
+                      aux.numero = int.parse(inValue ?? "0");
+                    },
+                  ),
+              ),
+              ElevatedButton(
+                child: const Text("Salvar"),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    print(aux);
+                  }
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
