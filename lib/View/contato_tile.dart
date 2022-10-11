488573151e49192bbb835c03775710eda87f06b1
davi133 <dqavi13@gmail.com>
2022-10-11 03:33:41 -0300

    agr da pra editar contato com essa monstruosidade

--- lib/View/contato_tile.dart
+++ lib/View/contato_tile.dart
@@ -48,14 +48,16 @@ class ContactOptions extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
+        //BOTÃO DE EDITAR #######################################
         ListTile(
           leading: const Icon(
             Icons.edit,
             color: Colors.blue,
           ),
           title: const Text("Editar"),
-          onTap: () {},
+          onTap: () {Navigator.of(context).push(MaterialPageRoute(builder: (_){return EditContato(cont, onFinish: onChanged,);}));},
         ),
+        //BOTÃO DE DELETAR ###################################
         ListTile(
           leading: const Icon(
             Icons.delete,
@@ -94,6 +96,98 @@ class ContactOptions extends StatelessWidget {
     );
   }
 }
+
+class EditContato extends StatelessWidget {
+  EditContato(this.cont,{Key? key, this.onFinish = (pressedDefault)})
+      : super(key: key);
+  final Function onFinish;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  Contato aux = Contato("", 1);
+  Contato cont;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("novo contato")),
+      body: Container(
+        margin: const EdgeInsets.all(15.0),
+        padding: const EdgeInsets.all(3.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ConstrainedBox(
+                constraints: const BoxConstraints(
+                    minHeight: 80, minWidth: 300, maxWidth: 500),
+                child: SizedBox(
+                  child: TextFormField(
+                    initialValue: cont.apelido,
+                    keyboardType: TextInputType.text,
+                    decoration: const InputDecoration(
+                      labelText: 'Apelido',
+                    ),
+                    validator: (String? inValue) {
+                      if (inValue != null) {
+                        if (inValue.isEmpty) {
+                          return "Insira um apelido";
+                        }
+                      }
+                      return null;
+                    },
+                    onSaved: (String? inValue) {
+                      aux.apelido = inValue ?? "";
+                    },
+                  ),
+                ),
+              ),
+              ConstrainedBox(
+                constraints: const BoxConstraints(
+                    minHeight: 85, minWidth: 300, maxWidth: 500),
+                child: TextFormField(
+                  initialValue: "${cont.numero}",
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Número',
+                  ),
+                  validator: (String? inValue) {
+                    if (inValue != null) {
+                      if (inValue.isEmpty) {
+                        return "Insira um número de contato";
+                      }
+                      if (!RegExp(r'^[0-9]+$').hasMatch(inValue)) {
+                        return "Insira Apenas números";
+                      }
+                    }
+                    return null;
+                  },
+                  onSaved: (String? inValue) {
+                    aux.numero = int.parse(inValue ?? "0");
+                  },
+                ),
+              ),
+              ElevatedButton(
+                child: const Text("Salvar"),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    editContato(cont.numero, aux);
+                    Navigator.of(context).pop();
+                    Navigator.of(context).pop();
+                    onFinish();
+                  }
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
 
 void pressedDefault() {
   return;
